c166f51bb69f2562fb44a4c43729f935ef03164d
Krom, Robertus <Robertus.Krom@ing.de>
2019-04-09 14:42:16 +0200

    Fixed license text [skip ci]

--- src/Dapplo.Microsoft.Extensions.Hosting.Plugins/Internals/AssemblyLoadContextExtensions.cs
+++ src/Dapplo.Microsoft.Extensions.Hosting.Plugins/Internals/AssemblyLoadContextExtensions.cs
@@ -1,23 +1,23 @@
-// Dapplo - building blocks for desktop applications
-// Copyright (C) 2019 Dapplo
+//  Dapplo - building blocks for desktop applications
+//  Copyright (C) 2019 Dapplo
 // 
-// For more information see: http://dapplo.net/
-// Dapplo repositories are hosted on GitHub: https://github.com/dapplo
+//  For more information see: http://dapplo.net/
+//  Dapplo repositories are hosted on GitHub: https://github.com/dapplo
 // 
-// This file is part of Dapplo.Microsoft.Extensions.Hosting.Samples
+//  This file is part of Dapplo.Microsoft.Extensions.Hosting
 // 
-// Dapplo.Microsoft.Extensions.Hosting.Samples is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
+//  Dapplo.Microsoft.Extensions.Hosting is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
 // 
-// Dapplo.Microsoft.Extensions.Hosting.Samples is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Lesser General Public License for more details.
+//  Dapplo.Microsoft.Extensions.Hosting is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
 // 
-// You should have a copy of the GNU Lesser General Public License
-// along with Dapplo.Microsoft.Extensions.Hosting.Samples. If not, see <http://www.gnu.org/licenses/lgpl.txt>.
+//  You should have a copy of the GNU Lesser General Public License
+//  along with Dapplo.Microsoft.Extensions.Hosting. If not, see <http://www.gnu.org/licenses/lgpl.txt>.
 
 using System.Reflection;
 using System.Runtime.Loader;
