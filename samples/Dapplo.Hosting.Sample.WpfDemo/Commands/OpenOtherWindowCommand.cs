1bce439516ac09fba691bf914527d0ea9fd08520
Tyler Gordon <tyler.gordon@simusa.org>
2020-01-10 16:12:17 -0500

    Updated the sample WPF application to utilize the new view model pattern.

---
+++ samples/Dapplo.Hosting.Sample.WpfDemo/Commands/OpenOtherWindowCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Input;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Dapplo.Hosting.Sample.WpfDemo.ViewModels;
+
+namespace Dapplo.Hosting.Sample.WpfDemo.Commands {
+
+  /// <summary>
+  /// A simple command which opens <see cref="OtherWindow"/> using the <see cref="OtherWindowViewModel"/> view model.
+  /// </summary>
+  public class OpenOtherWindowCommand : ICommand {
+    private readonly ILogger<OpenOtherWindowCommand> _logger;
+    private readonly IServiceProvider _serviceProvider;
+
+    public event EventHandler CanExecuteChanged;
+
+    public OpenOtherWindowCommand(
+      ILogger<OpenOtherWindowCommand> logger,
+      IServiceProvider serviceProvider
+      ) {
+      _logger = logger;
+      _serviceProvider = serviceProvider;
+    }
+
+    public bool CanExecute(object parameter) => true;
+
+    public void Execute(object parameter) {
+      _logger.LogInformation("Opening {0} with ViewModel {1}", nameof(OtherWindow), nameof(OtherWindowViewModel));
+      var window = _serviceProvider.GetService<OtherWindow>();
+      var viewModel = _serviceProvider.GetService<OtherWindowViewModel>();
+      window.DataContext = viewModel;
+      window.Show();
+      window.Focus();
+    }
+  }
+}
---
+++ samples/Dapplo.Hosting.Sample.WpfDemo/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Input;
+using Dapplo.Hosting.Sample.WpfDemo.Commands;
+using Dapplo.Microsoft.Extensions.Hosting.Wpf;
+using Microsoft.Extensions.Logging;
+
+namespace Dapplo.Hosting.Sample.WpfDemo.ViewModels {
+    /// <summary>
+    /// A simple view model
+    /// </summary>
+    public class MainWindowViewModel : IWpfViewModel
+    {
+        private readonly ILogger<MainWindowViewModel> _logger;
+        private readonly IServiceProvider _serviceProvider;
+
+        public MainWindowViewModel(
+          ILogger<MainWindowViewModel> logger,
+          IServiceProvider serviceProvider,
+          OpenOtherWindowCommand openOtherWindowCommand
+          )
+        {
+            _logger = logger;
+            _serviceProvider = serviceProvider;
+            Message = "Hello .NET Core!";
+            OpenOtherWindowCommand = openOtherWindowCommand;
+        }
+
+        public string Message { get; }
+
+        public ICommand OpenOtherWindowCommand { get; }
+    }
+}
